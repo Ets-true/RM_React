650ae8e71e6a1897363874fab1b79d8307e7e0b1
Kirill Smirnov <clean.energy.flow@gmail.com>
2022-08-13 12:05:37 +0300

    start changing techno page

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,8 @@ function send() {
     configFTP.log = util.log;
     const ftpConnect = vinylFTP.create(configFTP);
     gulp.src(`build/**/*.*`, {})
-        .pipe(ftpConnect.dest(`RM/public_html/`));
+        // .pipe(ftpConnect.dest(`RM/public_html/`))
+        .pipe(ftpConnect.dest(`Regeneration-oil/public_html/`));
 
     
 }
