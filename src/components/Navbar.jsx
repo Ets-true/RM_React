0ec882a7cdaa0c047984e262e9a20aeae59df9f9
Kirill Smirnov <clean.energy.flow@gmail.com>
2022-11-21 21:11:44 +0300

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import '../scss/header.scss'
 import logo from '../media/img/logo_full.png'
 import $ from 'jquery'
-import {location} from 'jquery'
 
 
 export default function Navbar() {
+
+  const defaultPagesList = [
+    {name: 'Главная', url: '/'},
+    {name: 'О компании', url: '/company'},
+    {name: 'Установки', url: '/plants'},
+    {name: 'Контакты', url: '/contact'},
+    // {name: 'Технология', url: '/techno'},
+    // {name: 'Экономика', url: '/economic'},
+    // {name: 'Архив', url: '/archive'},
+  ]
+
+  const [Pages, setPages] = useState(defaultPagesList)
+
+  useEffect(()=>{
+
+  },[Pages])
+
+  const changepage = (page) => {
+    let copyPages = JSON.parse(JSON.stringify(defaultPagesList))
+    let pageIndex
+    copyPages.forEach((copyPage, i)=> {
+      if(copyPage.name == page.name) pageIndex = i 
+    });
+    console.log(page)
+    console.log(pageIndex)
+    copyPages.splice(0, 0, copyPages.splice(pageIndex, 1)[0]);
+    setPages([...copyPages])
+  }
+
   return (
     <div class="header">
-      <div class="header__content">
-        <div class="header__column _l">
-          <Link to="/" className="header__link">Главная страница</Link>
-          <Link to="/company" className="header__link">О компании</Link>
-        </div>
-        <div class="header__logo">
-          <Link to="/">
-            <img src={logo} alt="" class="header__logo_img"></img>
+      <div class="header-content">
+        <div class="header-logo">
+          <Link to="/" onClick={() => changepage({name: 'Главная', url: '/'})}>
+            <img src={logo} alt=""></img>
           </Link>
         </div>
-        <div class="header__column _r">
-          <Link to="/techno" className="header__link">Технология</Link>
-          <Link to="/economic" className="header__link">Экономика</Link>
-          <Link to="/contact" className="header__link">Контакты</Link>
+
+        <div className="header-links">
+          {
+            Pages.map((page,i) =>{
+              if(i == 0){
+                return <Link to={page.url} className="header-link header-link-active" key={`link-${i}`} onClick={() => changepage(page)}>{page.name}</Link>
+              } else return <Link to={page.url} className="header-link" key={`link-${i}`} onClick={() => changepage(page)}>{page.name}</Link>
+            })
+          }
+          <div className="openClose-wrap">
+            <div className="openClose"></div>
+          </div>
         </div>
       </div>
     </div>
